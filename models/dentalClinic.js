b96acba40f56c1fbc3f4584a0ac590b4e60aa8fa
livm <livm@student.chalmers.se>
2020-11-25 14:46:27 +0100

    Mongoose schema for dental clinic DB objects

---
+++ models/dentalClinic.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const dentalClinicSchema = new Schema({
+  // Data for end-user authentication
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  // Data for dental clinic information
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  owner: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  // TODO: Require dentist schema
+  dentists: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'dentist',
+    },
+  ],
+  openingHours: [
+    {
+      day: String,
+      periods: [
+        {
+          start_hours: Date,
+          end_hours: Date,
+        },
+      ],
+    },
+  ],
+  // Location information
+  streetAddress: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  postcode: {
+    type: Number,
+    require: true,
+    trim: true,
+    max: 5,
+    min: 5,
+  },
+  city: {
+    type: String,
+    require: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('dentalClinic', dentalClinicSchema)
