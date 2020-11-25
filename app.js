10e85577248bb8e2056b4786087dd486cb806857
livm <livm@student.chalmers.se>
2020-11-25 14:12:48 +0100

    WS running off of app http

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const ws = require('ws')
 
 require('dotenv').config()
 
 const app = express()
+const wss = new ws.Server({ noServer: true })
 const port = process.env.PORT || 5050
 
 app.use(express.json())
@@ -20,6 +22,22 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully 🥳')
 })
 
-app.listen(port, () => {
+// Logic to run server
+wss.on('connection', socket => {
+  console.log('A new client connected 👀')
+  socket.send('Hello new client!')
+  socket.on('message', function incoming(message) {
+    console.log(`Received message: ${message}`)
+    socket.send(`Message received: ${message}`)
+  })
+})
+
+const server = app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
+})
+
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, socket => {
+    wss.emit('connection', socket, request)
+  })
 })
