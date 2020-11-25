0e8ef5323571fdfe80e842b7bbc699f8a56c83c1
livm <livm@student.chalmers.se>
2020-11-25 15:15:22 +0100

    Changed to shorter name for post function

--- controllers/dentalClinics.js
+++ controllers/dentalClinics.js
@@ -2,7 +2,7 @@ const DentalClinic = require('../models/dentalClinic')
 
 module.exports = {
   // Logic to create a new dental clinic
-  createNewDentalClinic: async (req, res, next) => {
+  createDentalClinic: async function(req, res, next) {
     try {
       const dentalClinic = new DentalClinic(req.value.body)
       await dentalClinic.save()
