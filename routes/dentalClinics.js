6bd203ca3021e06da8ecbd080464e22d668fe1ee
livm <livm@student.chalmers.se>
2020-11-25 15:45:11 +0100

    Get all included in router

--- routes/dentalClinics.js
+++ routes/dentalClinics.js
@@ -3,6 +3,8 @@ const dentalClinics = require('../controllers/dentalClinics')
 
 const router = express.Router()
 
-router.route('/').post(dentalClinics.createDentalClinic)
+router.route('/')
+  .post(dentalClinics.createDentalClinic)
+  .get(dentalClinics.getAllClinics)
 
 module.exports = router
