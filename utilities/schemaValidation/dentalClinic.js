7cb1dddb9a570cdcd9d405ddf013ab93fce33b10
livm <livm@student.chalmers.se>
2020-11-27 20:40:47 +0100

    Schema validation for dental clinic mongoose

---
+++ utilities/schemaValidation/dentalClinic.js
@@ -0,0 +1,28 @@
+const Joi = require('joi')
+
+module.exports = {
+  // Validation for new dental clinics
+  newDentalClinicSchema: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+    name: Joi.string().required(),
+    owner: Joi.string().required(),
+  }),
+
+  // Validation for setting a clinic's opening hours
+  openingHoursSchema: Joi.object().keys({
+    openingHours: Joi.array().items(
+      Joi.object({
+        day: Joi.string().valid(
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday'
+        ),
+        opening: Joi.date(),
+        closing: Joi.date(),
+      })
+    ),
+  }),
+}
